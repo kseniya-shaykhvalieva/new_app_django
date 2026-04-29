1cff1b4add20cb220e05e0ae169faf59782201ea
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-04-29 10:29:05 +0500

    Внесены настройки для подключения СУБД PostgreSQL

--- config/settings.py
+++ config/settings.py
@@ -1,11 +1,15 @@
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-SECRET_KEY = 'django-insecure-1vu^abe4#sw9)()nra0s#hx6^azmu+@x$m9ozdzla+sb=^y#&*'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
-DEBUG = True
+DEBUG = True if os.getenv('DEBUG') == True else False
 
 ALLOWED_HOSTS = ["*"]
 
@@ -50,8 +54,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT', default='5432'),
     }
 }
 
