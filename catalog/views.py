ba40b8573f0cf465d9ae4236d2200f4cad65adbc
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-05-05 13:07:20 +0500

    Создан контроллер информации о продукте

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
 
+from catalog.models import Product
+
+
 def home(request):
     return render(request,'home.html')
 
 def contacts(request):
     return render(request, 'contacts.html')
+
+def product_info(request, product_id):
+    product = Product.objects.get(id=product_id)
+    context = {"product" : product}
+    return render(request, "product_info.html", context)
