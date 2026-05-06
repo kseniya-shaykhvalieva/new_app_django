ead1992d5d57d9a4301f048fba142b747c3af318
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-05-07 00:00:55 +0500

    Реализована валидная форма добавления товара с сохранением в БД

--- catalog/views.py
+++ catalog/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import get_object_or_404, redirect, render
+
 from django.shortcuts import render
 
+from catalog.forms import ProductForm
 from catalog.models import Product
 
 
@@ -12,6 +15,17 @@ def contacts(request):
     return render(request, 'contacts.html')
 
 def product_detail(request, pk):
-    product = Product.objects.get(pk=pk)
+    product = get_object_or_404(Product, pk=pk)
     context = {"product" : product}
     return render(request, "product_detail.html", context)
+
+
+def product_create(request):
+    if request.method == "POST":
+        form = ProductForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect("catalog:home")
+    else:
+        form = ProductForm()
+    return render(request, "product_form.html", {"form": form})
