4582970f4742d6f3ec2a36ecfdffe63f174b1e0d
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-05-09 13:41:09 +0500

    Создана модель Blog

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    title = models.CharField(max_length=250, verbose_name="Заголовок")
+    content = models.TextField(verbose_name="Содержимое")
+    preview = models.ImageField(
+        upload_to="images/", null=True, blank=True, verbose_name="Превью", help_text="Загрузите изображение"
+    )
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
+    is_published = models.BooleanField(default=False, verbose_name="Опубликовано")
+    views_count = models.PositiveIntegerField(verbose_name="Количество просмотров", default=0)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "заголовок"
+        verbose_name_plural = "заголовки"
+        ordering = ["title", "created_at"]
