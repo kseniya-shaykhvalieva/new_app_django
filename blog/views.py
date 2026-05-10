92e2b26cc29555c12802b727d9d26753943174d2
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-05-10 15:08:56 +0500

    Добавлено: Увеличение счетчика просмотров

--- blog/views.py
+++ blog/views.py
@@ -17,6 +17,12 @@ class BlogCreateView(CreateView):
 class BlogDetailView(DetailView):
     model = Blog
 
+    def get_object(self, queryset = None):
+        self.object = super().get_object(queryset)
+        self.object.views_count += 1
+        self.object.save()
+        return self.object
+
 
 class BlogUpdateView(UpdateView):
     model = Blog
@@ -27,4 +33,3 @@ class BlogUpdateView(UpdateView):
 class BlogDeleteView(DeleteView):
     model = Blog
     success_url = reverse_lazy('blog:blog_list')
-
