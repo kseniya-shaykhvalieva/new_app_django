993c4adb46203faabb3a171c61426f7400aa1edd
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-05-10 15:16:41 +0500

    Добавлено: Перенаправление после редактирования

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView, DetailView, UpdateView, DeleteView
 
 from blog.models import Blog
@@ -28,6 +28,9 @@ class BlogUpdateView(UpdateView):
     model = Blog
     fields = ("title", "content", "preview", "is_published")
     success_url = reverse_lazy('blog:blog_list')
+
+    def get_success_url(self):
+        return reverse('blog:blog_detail', args=[self.kwargs.get('pk')])
 
 
 class BlogDeleteView(DeleteView):
