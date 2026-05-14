1a16005c97b395d65fd0c9f02b03a8faa92f07a4
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-05-14 15:08:31 +0500

    Реализована валидация формы от спама

--- catalog/forms.py
+++ catalog/forms.py
@@ -4,9 +4,20 @@ from catalog.models import Product
 
 
 class ProductForm(forms.ModelForm):
+    FORBIDDEN_WORDS = ['казино', 'криптовалюта', 'крипта', 'биржа', 'дешево', 'бесплатно', 'обман', 'полиция', 'радар']
+
     class Meta:
         model = Product
         exclude = ("created_at", "updated_at",)
+
+    def clean(self):
+        cleaned_data = super().clean()
+        name = cleaned_data.get('name')
+        description = cleaned_data.get('description')
+
+        for word in self.FORBIDDEN_WORDS:
+            if word in name.lower() or word in description.lower():
+                self.add_error('name', f'Слово {word} - запрещено!')
 
     def clean_price(self):
         price = self.cleaned_data["price"]
