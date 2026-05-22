7db5031a89ebae0510bfa29ffbaa13fd0a3e5168
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-05-22 18:20:20 +0500

    Исправлено: username в форме не нужен,т.к. в модели None

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
-from django import forms
 
 from users.models import CustomUser
 
 
 class UserRegisterForm(UserCreationForm):
-    username = forms.CharField(max_length=50, required=True)
     usable_password = None
 
     class Meta:
