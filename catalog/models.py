f837059c293dcae42317a0c0f76d0f8329012781
Kseniya Shaykhvalieva <kseniyashajhvalieva@gmail.com>
2026-05-28 14:59:16 +0500

    Обновлена мадель Product, применены миграции

--- catalog/models.py
+++ catalog/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from users.models import CustomUser
+
 
 class Category(models.Model):
     name = models.CharField(max_length=150, verbose_name="Наименование")
@@ -19,6 +21,8 @@ class Product(models.Model):
     price = models.IntegerField(verbose_name="Цена за покупку")
     created_at = models.DateTimeField(auto_now_add=True, verbose_name="Дата создания")
     updated_at = models.DateTimeField(auto_now=True, verbose_name="Дата последнего изменения")
+    owner = models.ForeignKey(CustomUser, on_delete=models.CASCADE, verbose_name="Владелец")
+    is_published = models.BooleanField(default=False, verbose_name="Опубликовано", help_text="Отметьте, чтобы опубликовать продукт")
 
     def __str__(self):
         return self.name
@@ -27,3 +31,6 @@ class Product(models.Model):
         verbose_name = "продукт"
         verbose_name_plural = "продукты"
         ordering = ["category", "name"]
+        permissions = [
+            ("can_unpublish_product", "Can unpublish product"),  # может отменять публикацию продукта
+        ]
